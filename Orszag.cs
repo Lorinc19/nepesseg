7d6e790b66bbd379631f1c0ec839ce4b5d8b7dec
Lőrinc19 <locics@gmail.com>
2024-09-25 20:28:26 +0200

    Orszag constuctor ready

--- Orszag.cs
+++ Orszag.cs
@@ -6,13 +6,24 @@ using System.Threading.Tasks;
 
 namespace nepesseg
 {
-    internal class Orszag
+    public class Orszag
     {
         public string orszag { get; set; }
         public int terulet { get; set; }
         public int nepesseg { get; set; }
-        public string fovaros {  get; set; }
+        public string fovaros { get; set; }
         public int fmepesseg { get; set; }
 
+
+
+        public Orszag(string sor)
+        {
+            string[] darab = sor.Split(';');
+            this.orszag = darab[0];
+            this.terulet= Convert.ToInt32(darab[1]);
+            this.nepesseg = Convert.ToInt32(darab[2]);
+            this.fovaros = darab[3];
+            this.fmepesseg = Convert.ToInt32(darab[4]);
+        }
     }
 }
