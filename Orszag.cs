8564fa76f453548870741384c340981a2c631649
Lőrinc19 <locics@gmail.com>
2024-09-25 20:17:40 +0200

    Orszag class is ready to use

---
+++ Orszag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nepesseg
+{
+    internal class Orszag
+    {
+        public string orszag { get; set; }
+        public int terulet { get; set; }
+        public int nepesseg { get; set; }
+        public string fovaros {  get; set; }
+        public int fmepesseg { get; set; }
+
+    }
+}
